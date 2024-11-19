72f2ca86b0e0b2c3d58c3b833802d6d5e1b01228
bauyrzhan <newnewernewest123@gmail.com>
2024-11-20 00:13:51 +0500

    viewsets

--- app/views.py
+++ app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+from .models import *
+from rest_framework import permissions, viewsets
+from .serializers import *
 
 def view_mes(request):
     return HttpResponse("Hello, world. You're at the polls view.")
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
