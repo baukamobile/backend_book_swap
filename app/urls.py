0276e23050cbf3abafbc0b41021c703c41e7e8a1
bauyrzhan <newnewernewest123@gmail.com>
2024-11-21 21:48:00 +0500

    urls

--- app/urls.py
+++ app/urls.py
@@ -1,24 +1,15 @@
-from django.urls import path,include
-from . import views
-from rest_framework import routers
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, BookViewSet, TransactionViewSet, ExchangeViewSet, WishlistViewSet
 
-# router = routers.DefaultRouter()
-# router.register(r'profile', views.UserProfileViewSet)
-# router.register(r'books', views.BookViewSet)
-# # router.register(r'account', UserProfileViewSet)
-#
-# urlpatterns = [
-#     path('func/', views.view_mes),
-#     path('api-auth/', include('rest_framework.urls')),
-#     # path('books', include('rest_framework.urls')),
-# ]
-
-router = routers.DefaultRouter()
-router.register(r'profile', views.UserProfileViewSet)
-router.register(r'books', views.BookViewSet)
+# Create a router and register your viewsets
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'books', BookViewSet)
+router.register(r'transactions', TransactionViewSet)
+router.register(r'exchanges', ExchangeViewSet)
+router.register(r'wishlist', WishlistViewSet)
 
 urlpatterns = [
-    path('func/', views.view_mes),
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/', include(router.urls)),  # Include the router's URLs
+    path('api/', include(router.urls)),
 ]
