37dfa8ec275dedf33ed0fb790d88f12fdaa93120
bauyrzhan <newnewernewest123@gmail.com>
2024-12-03 15:58:37 +0500

    user urls

--- app/urls.py
+++ app/urls.py
@@ -2,14 +2,9 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import (UserViewSet,
-                    BookViewSet,
-                    TransactionViewSet,
-                    ExchangeViewSet,
-                    WishlistViewSet,
-                     get_users,
-                    )
-from .views import CustomTokenObtainPairView, CustomTokenRefreshView, logout, register
+from .views import *
+
+
 # Create a router and register your viewsets
 router = DefaultRouter()
 router.register(    r'users', UserViewSet)
@@ -24,9 +19,9 @@ urlpatterns = [
                   path('register/', register.as_view()),
                   path('login/', loginView.as_view()),
                   path('user/', userget.as_view()),
-                  path('logout/', logoutView.as_view())
-    path('api/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
-    path('api/users/', get_users),
+                  path('logout/', logoutView.as_view()),
+                  path('api/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
+                  path('api/users/', get_users),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
