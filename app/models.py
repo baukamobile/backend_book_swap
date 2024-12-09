52149bcbc159e1bd5f78ffcd93f3ca43044be9be
bauyrzhan <newnewernewest123@gmail.com>
2024-12-09 15:55:04 +0500

    cascade

--- app/models.py
+++ app/models.py
@@ -115,7 +115,7 @@ class Chat(models.Model):
         return f"Chat {self.id} - Members: {', '.join([p.email for p in self.participants.all()])}"
 
 class Message(models.Model):
-    chat = models.ForeignKey(Chat, on_delete=models.CASCADE(), related_name="messages")
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name="messages")
     sender = models.ForeignKey(CustomUser,on_delete=models.CASCADE, related_name="sent_messages")
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
