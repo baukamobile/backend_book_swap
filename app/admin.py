b2ac49fae11c223c16023b222d2e0c6b501b0822
bauyrzhan <newnewernewest123@gmail.com>
2024-12-19 21:04:42 +0500

    message models added receipt field

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import CustomUser,Exchange, Book,Transaction,Wishlist,RegionUser,Genres
+from .models import CustomUser, Exchange, Book, Transaction, Wishlist, RegionUser, Genres, Message, Chat
 
 admin.site.register(CustomUser)
 admin.site.register(RegionUser)
@@ -15,4 +15,6 @@ admin.site.register(Transaction)
 admin.site.register(Wishlist)
 admin.site.register(Genres)
 
+admin.site.register(Message)
+admin.site.register(Chat)
 
--- app/models.py
+++ app/models.py
@@ -138,9 +138,13 @@ class Chat(models.Model):
 class Message(models.Model):
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name="messages")
     sender = models.ForeignKey(CustomUser,on_delete=models.CASCADE, related_name="sent_messages")
+    recipient = models.ForeignKey(CustomUser, related_name='received_messages', on_delete=models.CASCADE, null=True)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"From {self.sender} to {self.recipient} at {self.timestamp}"
 
 
 
