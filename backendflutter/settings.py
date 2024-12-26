ed3516b32b564ec65d4ae667d8fbf89725472270
bauyrzhan <newnewernewest123@gmail.com>
2024-12-26 13:58:40 +0500

    we should copy 412be6b to heroku4

--- backendflutter/settings.py
+++ backendflutter/settings.py
@@ -56,7 +56,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 63979)],
+            "hosts": [("127.0.0.1", 6379)],
         },
     },
 }
