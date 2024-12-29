a47bcf70ee405dd0bf53e40039843b2ce2573143
bauyrzhan <newnewernewest123@gmail.com>
2024-12-29 19:39:51 +0500

    requirements.txt added environ package

--- backendflutter/settings.py
+++ backendflutter/settings.py
@@ -202,8 +202,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 django_heroku.settings(locals())
 # For serving media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
