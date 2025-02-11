11281147ffd2d0b5ee35a9d85066853ada7b45f9
bauyrzhan <newnewernewest123@gmail.com>
2025-02-11 18:38:32 +0500

    message model rest api

--- chat/models.py
+++ chat/models.py
@@ -9,8 +9,11 @@ class Chat(models.Model):
         return f"Chat {self.id} - Members: {', '.join([p.email for p in self.participants.all()])}"
 
 class Message(models.Model):
-    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name="messages")
-    sender = models.ForeignKey(CustomUser,on_delete=models.CASCADE, related_name="sent_messages")
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE,null=True, related_name="messages")
+    sender = models.ForeignKey(CustomUser,on_delete=models.CASCADE,null=True, related_name="sent_messages")
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.sender}: {self.content}"
